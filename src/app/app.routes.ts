fdb3a98037c4b26ad7b36d0e17ca968f69115367
bentoroal <ben.toroal@gmail.com>
2025-10-20 23:29:09 -0300

    Agrega secciones y sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,18 @@
 import { Routes } from '@angular/router';
 import { AboutMeComponent } from './features/aboutme/aboutme';
+import { SkillsComponent } from './features/skills/skills';
+import { ExperienceComponent } from './features/experience/experience';
+import { EducationComponent } from './features/education/education';
+import { ContactComponent } from './features/contact/contact';
+import { KnowledgeComponent } from './features/knowledge/knowledge';
 
 export const routes: Routes = [
-  { path: 'about', component: AboutMeComponent }
+  { path: '', redirectTo: 'about', pathMatch: 'full' },
+  { path: 'about', component: AboutMeComponent },
+  { path: 'education', component: EducationComponent },
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'knowledge', component: KnowledgeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: 'about' }
 ];
---
+++ src/app/features/knowledge/knowledge.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-knowledge',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './knowledge.html',
+  styleUrls: ['./knowledge.scss']
+})
+export class KnowledgeComponent {}
